21e4e87e3e4d71c743d6517355f102e57f7233d8
Sarath <vssarathc04@gmail.com>
2023-12-28 05:27:27 +0530

    color generation using pastel colors

--- src/data/utils.js
+++ src/data/utils.js
@@ -4,15 +4,34 @@ function isEmpty(obj) {
     return Object.keys(obj).length === 0;
 }
 
+function hsl2rgb(h, s, l) {
+    const r = Math.round(255 * (Math.cos(h * Math.PI / 180) + 1) / 2 * s * l);
+    const g = Math.round(255 * (1 - Math.abs(Math.cos((h + 120) * Math.PI / 180)) / 2) * s * l);
+    const b = Math.round(255 * (1 - Math.abs(Math.cos((h + 240) * Math.PI / 180)) / 2) * s * l);
+    return [r, g, b];
+}
+
+function getRandomPastelColor() {
+    // Generate a random hue between 0 and 360 degrees, ensuring a pastel range
+    const hue = Math.random() * 360; // Skew towards pastel hues
+
+    // Set saturation and lightness within a pastel range
+    const saturation = 65 + Math.random() * 10; // Low to moderate saturation
+    const lightness = 75 + Math.random() * 15; // Moderate to high lightness
+
+    // Convert HSV to RGB using a concise formula
+    const rgb = hsl2rgb(hue, saturation / 100, lightness / 100);
+
+    return rgb.map(x => x.toString(16).padStart(2, '0')).join('');
+}
+
 function getSubjectColorDict(subjectNameArr) {
     const subjectColorDict = {};
     for (const subj of subjectNameArr) {
         let clr = localStorage.getItem(subj);
         if (clr == null) {
-            clr = Math.floor(Math.random() * 16777215)
-                .toString(16)
-                .padStart(6, "0")
-                .toUpperCase();
+            clr = getRandomPastelColor();
+            clr = clr.slice(0, 6) + "FF";
 
             localStorage.setItem(subj, clr);
         }
@@ -20,6 +39,11 @@ function getSubjectColorDict(subjectNameArr) {
     }
     return subjectColorDict;
 }
+
+const withOpacity = (color, opacity) => {
+    const [r, g, b] = color.match(/\w\w/g).map((x) => parseInt(x, 16));
+    return `rgba(${r},${g},${b},${opacity})`;
+};
 
 function findCommonSlot(subTimeSlotArr, subSlotArr) {
     for (const timeSlot of subTimeSlotArr) {
@@ -72,6 +96,7 @@ function getActualSlotDict(subTimeSlotDict, subjectSlotDict) {
 
 export { 
     isEmpty,
+    withOpacity,
     isInDisabledSlots,
     getActualSlotDict,
     getMaskedSubSlotDict,

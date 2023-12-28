f1a9d0d7b2e0dcb669ad0960f517767604e07d20
Sarath <vssarathc04@gmail.com>
2023-12-28 22:02:17 +0530

    lab data updated

--- src/data/lab_data.js
+++ src/data/lab_data.js
@@ -1,6 +1,6 @@
 const labData =
 {
-    "CSE1005-Software Engineering": [
+    "CSE1005-Software Engineering (LAB)": [
         "L27+L28",
         "L35+L36",
         "L19+L20",
@@ -31,7 +31,7 @@ const labData =
         "L51+L52",
         "L41+L42"
     ],
-    "CSE1006-Foundations for Data Analytics": [
+    "CSE1006-Foundations for Data Analytics (LAB)": [
         "L23+L24",
         "L27+L28",
         "L5+L6",
@@ -45,20 +45,20 @@ const labData =
         "L41+L42",
         "L47+L48"
     ],
-    "CSE1007-Introduction to Cryptography": [
+    "CSE1007-Introduction to Cryptography (LAB)": [
         "L51+L52",
         "L7+L8",
         "L37+L38"
     ],
-    "CSE1020-Procedural Programming Using C": [
+    "CSE1020-Procedural Programming Using C (LAB)": [
         "L47+L48",
         "L43+L44",
         "L15+L16"
     ],
-    "CSE2006-Computer Graphics": [
+    "CSE2006-Computer Graphics (LAB)": [
         "L9+L10"
     ],
-    "CSE2007-Database Management Systems": [
+    "CSE2007-Database Management Systems (LAB)": [
         "L27+L28",
         "L35+L36",
         "L19+L20",
@@ -89,12 +89,12 @@ const labData =
         "L51+L52",
         "L41+L42"
     ],
-    "CSE2011-Cyber Security and Digital Forensics": [
+    "CSE2011-Cyber Security and Digital Forensics (LAB)": [
         "L3+L4",
         "L41+L42",
         "L39+L40"
     ],
-    "CSE3002-Artificial Intelligence": [
+    "CSE3002-Artificial Intelligence (LAB)": [
         "L27+L28",
         "L35+L36",
         "L19+L20",
@@ -125,7 +125,7 @@ const labData =
         "L51+L52",
         "L41+L42"
     ],
-    "CSE3003-Computer Networks": [
+    "CSE3003-Computer Networks (LAB)": [
         "L27+L28",
         "L35+L36",
         "L19+L20",
@@ -156,7 +156,7 @@ const labData =
         "L51+L52",
         "L41+L42"
     ],
-    "CSE3004-Design and Analysis of Algorithms": [
+    "CSE3004-Design and Analysis of Algorithms (LAB)": [
         "L27+L28",
         "L35+L36",
         "L19+L20",
@@ -184,38 +184,38 @@ const labData =
         "L57+L58",
         "L51+L52"
     ],
-    "CSE3006-Data Visualization": [
+    "CSE3006-Data Visualization (LAB)": [
         "L7+L8",
         "L53+L54",
         "L15+L16",
         "L39+L40"
     ],
-    "CSE3008-Introduction to Machine Learning": [
+    "CSE3008-Introduction to Machine Learning (LAB)": [
         "L23+L24",
         "L49+L50"
     ],
-    "CSE3009-No SQL Databases": [
+    "CSE3009-No SQL Databases (LAB)": [
         "L53+L54",
         "L29+L30",
         "L25+L26",
         "L31+L32",
         "L9+L10"
     ],
-    "CSE3011-Network Programming": [
+    "CSE3011-Network Programming (LAB)": [
         "L27+L28",
         "L55+L56",
         "L53+L54"
     ],
-    "CSE3012-Network Security": [
+    "CSE3012-Network Security (LAB)": [
         "L25+L26",
         "L37+L38"
     ],
-    "CSE3013-Secure Group Communications": [
+    "CSE3013-Secure Group Communications (LAB)": [
         "L21+L22",
         "L43+L44",
         "L27+L28"
     ],
-    "CSE3015-Natural Language Processing": [
+    "CSE3015-Natural Language Processing (LAB)": [
         "L27+L28",
         "L35+L36",
         "L3+L4",
@@ -235,13 +235,13 @@ const labData =
         "L57+L58",
         "L51+L52"
     ],
-    "CSE4005-Data Warehousing and Data Mining": [
+    "CSE4005-Data Warehousing and Data Mining (LAB)": [
         "L51+L52",
         "L25+L26",
         "L41+L42",
         "L11+L12"
     ],
-    "CSE4007-Digital Image Processing": [
+    "CSE4007-Digital Image Processing (LAB)": [
         "L27+L28",
         "L35+L36",
         "L13+L14",
@@ -257,23 +257,23 @@ const labData =
         "L47+L48",
         "L9+L10"
     ],
-    "CSE4008-Social Network Analysis": [
+    "CSE4008-Social Network Analysis (LAB)": [
         "L23+L24",
         "L19+L20",
         "L1+L2",
         "L57+L58",
         "L47+L48"
     ],
-    "CSE4016-Database Administration": [
+    "CSE4016-Database Administration (LAB)": [
         "L17+L18",
         "L49+L50",
         "L3+L4"
     ],
-    "CSE4031-Web Application Security": [
+    "CSE4031-Web Application Security (LAB)": [
         "L19+L20",
         "L49+L50"
     ],
-    "CSE4037-Reinforcement Learning": [
+    "CSE4037-Reinforcement Learning (LAB)": [
         "L23+L24",
         "L35+L36",
         "L19+L20",
@@ -286,75 +286,75 @@ const labData =
         "L9+L10",
         "L45+L46"
     ],
-    "CSE4040-Mining Massive Data Sets": [
+    "CSE4040-Mining Massive Data Sets (LAB)": [
         "L17+L18",
         "L21+L22",
         "L57+L58"
     ],
-    "CSE4042-Blockchain Platforms and Applications": [
+    "CSE4042-Blockchain Platforms and Applications (LAB)": [
         "L55+L56"
     ],
-    "CSE4044-Software Defined Networks and Network Function Virtualization": [
+    "CSE4044-Software Defined Networks and Network Function Virtualization (LAB)": [
         "L39+L40"
     ],
-    "ECE1007-Sensor Systems": [
+    "ECE1007-Sensor Systems (LAB)": [
         "L55+L56"
     ],
-    "ECE1008-Sensors and Control Systems": [
+    "ECE1008-Sensors and Control Systems (LAB)": [
         "L51+L52"
     ],
-    "ECE2001-Analog Devices and Circuits": [
+    "ECE2001-Analog Devices and Circuits (LAB)": [
         "L7+L8",
         "L37+L38",
         "L35+L36",
         "L39+L40"
     ],
-    "ECE2006-Communication Systems": [
+    "ECE2006-Communication Systems (LAB)": [
         "L23+L24",
         "L51+L52",
         "L55+L56",
         "L3+L4"
     ],
-    "ECE2007-Digital Signal Processing": [
+    "ECE2007-Digital Signal Processing (LAB)": [
         "L11+L12"
     ],
-    "ECE2014-Microprocessors and Microcontrollers": [
+    "ECE2014-Microprocessors and Microcontrollers (LAB)": [
         "L41+L42",
         "L43+L44",
         "L31+L32",
         "L3+L4"
     ],
-    "ECE3001-Linear Integrated Circuits and Applications": [
+    "ECE3001-Linear Integrated Circuits and Applications (LAB)": [
         "L41+L42",
         "L33+L34",
         "L11+L12",
         "L9+L10"
     ],
-    "ECE3005-CMOS VLSI Design": [
+    "ECE3005-CMOS VLSI Design (LAB)": [
         "L51+L52"
     ],
-    "ECE3006-HDL Verification and Methodology": [
+    "ECE3006-HDL Verification and Methodology (LAB)": [
         "L31+L32"
     ],
-    "ECE3010-Speech processing": [
+    "ECE3010-Speech processing (LAB)": [
         "L33+L34"
     ],
-    "ECE4003-Embedded Programming": [
+    "ECE4003-Embedded Programming (LAB)": [
         "L27+L28"
     ],
-    "ECE4005-SOC Design": [
+    "ECE4005-SOC Design (LAB)": [
         "L21+L22"
     ],
-    "ECE4006-Analog IC Design": [
+    "ECE4006-Analog IC Design (LAB)": [
         "L3+L4"
     ],
-    "ECE4017-IoT Domain Analyst": [
+    "ECE4017-IoT Domain Analyst (LAB)": [
         "L29+L30"
     ],
-    "ECE4018-Image Analysis and Computer Vision": [
+    "ECE4018-Image Analysis and Computer Vision (LAB)": [
         "L51+L52"
     ],
-    "MAT1011-Applied Statistics": [
+    "MAT1011-Applied Statistics (LAB)": [
         "L27+L28",
         "L35+L36",
         "L19+L20",
@@ -385,41 +385,41 @@ const labData =
         "L51+L52",
         "L41+L42"
     ],
-    "MAT2001-Numerical Methods for Engineers": [
+    "MAT2001-Numerical Methods for Engineers (LAB)": [
         "L49+L50",
         "L3+L4"
     ],
-    "MAT2003-Optimization Techniques": [
+    "MAT2003-Optimization Techniques (LAB)": [
         "L27+L28",
         "L45+L46"
     ],
-    "MEC1002-Engineering Graphics": [
+    "MEC1002-Engineering Graphics (LAB)": [
         "L13+L14+L21+L22"
     ],
-    "MEC2005-Fundamentals of Manufacturing Processes": [
+    "MEC2005-Fundamentals of Manufacturing Processes (LAB)": [
         "L1+L2",
         "L5+L6"
     ],
-    "MEC3001-Heat and Mass Transfer": [
+    "MEC3001-Heat and Mass Transfer (LAB)": [
         "L37+L38"
     ],
-    "MEC3002-Theory of Machines": [
+    "MEC3002-Theory of Machines (LAB)": [
         "L1+L2",
         "L5+L6"
     ],
-    "MEC3005-Automation in Manufacturing": [
+    "MEC3005-Automation in Manufacturing (LAB)": [
         "L39+L40"
     ],
-    "MEC3012-Additive Manufacturing": [
+    "MEC3012-Additive Manufacturing (LAB)": [
         "L41+L42"
     ],
-    "MEC4003-Internal Combustion Engines": [
+    "MEC4003-Internal Combustion Engines (LAB)": [
         "L33+L34"
     ],
-    "MEC4008-Automobile Engineering": [
+    "MEC4008-Automobile Engineering (LAB)": [
         "L39+L40"
     ],
-    "MGT1001-Ethics and Values": [
+    "MGT1001-Ethics and Values (LAB)": [
         "L27+L28",
         "L49+L50",
         "L35+L36",
@@ -433,7 +433,7 @@ const labData =
         "L39+L40",
         "L9+L10"
     ],
-    "SWE2006-Database Systems": [
+    "SWE2006-Database Systems (LAB)": [
         "L49+L50",
         "L53+L54",
         "L19+L20",
@@ -443,7 +443,7 @@ const labData =
         "L9+L10",
         "L45+L46"
     ],
-    "SWE3001-Introduction to Computer Networks": [
+    "SWE3001-Introduction to Computer Networks (LAB)": [
         "L35+L36",
         "L3+L4",
         "L7+L8",
@@ -452,17 +452,17 @@ const labData =
         "L57+L58",
         "L5+L6"
     ],
-    "SWE3005-Principles of Design Patterns": [
+    "SWE3005-Principles of Design Patterns (LAB)": [
         "L41+L42",
         "L11+L12",
         "L39+L40"
     ],
-    "SWE4002-Software Testing": [
+    "SWE4002-Software Testing (LAB)": [
         "L29+L30",
         "L53+L54",
         "L45+L46"
     ],
-    "SWE4006-Advanced Server Side Programming": [
+    "SWE4006-Advanced Server Side Programming (LAB)": [
         "L29+L30",
         "L53+L54",
         "L45+L46"

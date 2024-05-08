bdcdb28c92a239b7f3d91d504637cfe50264a6e5
Sarath191181208 <vssarathc04@gmail.com>
2024-05-08 11:45:43 +0530

    fix(invalid export items): time_table.js

--- src/data/time_table.js
+++ src/data/time_table.js
@@ -1,17 +1,17 @@
-const time_table = {
-  "Monday":   [ "TBB+L1" , "TBB+L2", "TAA+L3", "TAA+L4", "TCC+L25", "TCC+L26", "TD+L27", "TD+L28" ],
-  "Tuesday":  [ "TC+L5"  , "TC+L6", "TB+L7", "TB+L8", "TA+L29", "TA+L30", "D+L31", "D+L32" ],
-  "Wednesday":[ "B+L9"   , "B+L10", "A+L11", "A+L12", "C+L33", "C+L34", "D+L35", "D+L36" ],
-  "Thursday": [ "B+L13"  , "B+L14", "C+L15", "C+L16", "A+L37", "A+L38", "D+L39", "D+L40" ],
-  "Friday":   [ "TA+L17" , "TA+L18", "TB+L19", "TB+L20", "TC+L41", "TC+L42", "D+L43", "D+L44" ],
-  "Saturday": [ "TBB+L21", "TBB+L22", "TCC+L23", "TCC+L24", "TAA+L45", "TAA+L46", "TD+L47", "TD+L48" ]
+export const time_table = {
+  "Monday": ["TBB+L1", "TBB+L2", "TAA+L3", "TAA+L4", "TCC+L25", "TCC+L26", "TD+L27", "TD+L28"],
+  "Tuesday": ["TC+L5", "TC+L6", "TB+L7", "TB+L8", "TA+L29", "TA+L30", "D+L31", "D+L32"],
+  "Wednesday": ["B+L9", "B+L10", "A+L11", "A+L12", "C+L33", "C+L34", "D+L35", "D+L36"],
+  "Thursday": ["B+L13", "B+L14", "C+L15", "C+L16", "A+L37", "A+L38", "D+L39", "D+L40"],
+  "Friday": ["TA+L17", "TA+L18", "TB+L19", "TB+L20", "TC+L41", "TC+L42", "D+L43", "D+L44"],
+  "Saturday": ["TBB+L21", "TBB+L22", "TCC+L23", "TCC+L24", "TAA+L45", "TAA+L46", "TD+L47", "TD+L48"]
 };
 
 
 /**
  * @type {string[]}
  */
-const time_arr = ["9:00", "10:00", "11:00", "12:00", "2:00", "3:00", "4:00", "5:00"];
+export const time_arr = ["9:00", "10:00", "11:00", "12:00", "2:00", "3:00", "4:00", "5:00"];
 
 const isExceptionSlot = (testSlot) => {
   const slots = [
@@ -30,7 +30,7 @@ const isExceptionSlot = (testSlot) => {
  * @param {string} slot 
  * @returns {number}
  */
-const getCreditsFromSlot = (slot) => {
+export const getCreditsFromSlot = (slot) => {
   const isLab = slot.startsWith("L");
   if (isLab) return 1;
   const numberOfPluses = slot.split("+").length - 1;

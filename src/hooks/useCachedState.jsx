bcf4f7ad131766b24354566f699b0b0b7e5b8c86
Sarath191181208 <vssarathc04@gmail.com>
2024-05-15 08:10:13 +0530

    versioning for app state

--- src/hooks/useCachedState.jsx
+++ src/hooks/useCachedState.jsx
@@ -1,18 +1,19 @@
 // Description: This hook is used to store data in local storage and retrieve it when needed
 import { useState } from "react";
 
-function useCachedState({cacheKey, defaultValue}) {
-    const [state, setState] = useState(() => {
-        const cachedState = localStorage.getItem(cacheKey);
-        return cachedState ? JSON.parse(cachedState) : defaultValue;
-    });
-    
-    const setStateWithCache = (value) => {
-        localStorage.setItem(cacheKey, JSON.stringify(value));
-        setState(value);
-    }
+function useCachedState({ cacheKey, defaultValue }) {
+  cacheKey += "V2";
+  const [state, setState] = useState(() => {
+    const cachedState = localStorage.getItem(cacheKey);
+    return cachedState ? JSON.parse(cachedState) : defaultValue;
+  });
 
-    return [state, setStateWithCache];
+  const setStateWithCache = (value) => {
+    localStorage.setItem(cacheKey, JSON.stringify(value));
+    setState(value);
+  }
+
+  return [state, setStateWithCache];
 }
 
 export { useCachedState };
